f873b48e9d57c30f4ac870ecbaa48565500173c6
TheCatwoman <taylor.sheppard@email.socc.edu>
2024-09-14 21:26:45 -0700

    Update ImageData.h

--- ImageData.h
+++ ImageData.h
@@ -3,11 +3,31 @@
 #ifndef _IMAGEDATA_H_
 #define _IMAGEDATA_H_
 
-extern const unsigned char epd_bitmap_Mouth_eyes_3[];
-extern const unsigned char epd_bitmap_Mouth_eyes_4[];
-extern const unsigned char epd_bitmap_Mouth_eyes_5[];
-extern const unsigned char epd_bitmap_Mouth_eyes_6[];
-extern const unsigned char epd_bitmap_Mouth_eyes_7[];
+  extern const unsigned char epd_bitmap_Bp_1[];
+	extern const unsigned char epd_bitmap_Bp_2[];
+	extern const unsigned char epd_bitmap_Bp_3[];
+	extern const unsigned char epd_bitmap_Bp_4[];
+	extern const unsigned char epd_bitmap_Bp_5[];
+	extern const unsigned char epd_bitmap_Bp_6[];
+	extern const unsigned char epd_bitmap_Bp_6a[];
+	extern const unsigned char epd_bitmap_Bp_7[];
+	extern const unsigned char epd_bitmap_Bp_7a[];
+	extern const unsigned char epd_bitmap_Bp_8a[];
+	extern const unsigned char epd_bitmap_Bp_m0[];
+	extern const unsigned char epd_bitmap_Bp_m1[];
+	extern const unsigned char epd_bitmap_Bp_m2[];
+	extern const unsigned char epd_bitmap_Bp_m2_1[];
+	extern const unsigned char epd_bitmap_Bp_m3[];
+	extern const unsigned char epd_bitmap_Bp_m4[];
+	extern const unsigned char epd_bitmap_Bp_m4_1[];
+	extern const unsigned char epd_bitmap_Bp_m5_1[];
+	extern const unsigned char epd_bitmap_Bp_m5_2[];
+	extern const unsigned char epd_bitmap_Bp_m_0[];
+	extern const unsigned char epd_bitmap_bitmap[];
+
+
+
+// extern const unsigned char epd_bitmap_Mouth_eyes_7[];
 
 
 #endif
